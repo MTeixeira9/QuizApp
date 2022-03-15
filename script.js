001a75682e9bd926e6b44b921ebc536837818586
MTeixeira9 <mario_ze16@hotmail.com>
2022-03-15 10:48:10 +0000

    Add structure to quiz

---
+++ script.js
@@ -0,0 +1,42 @@
+const quizData = [
+    {
+        question: 'How old id Florin?',
+        a: '10',
+        b: '17',
+        c: '26',
+        d: '110',
+        correct: 'c'
+    },
+    {
+        question: 'What is the most used programming language un 2019?',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'd'
+    },
+    {
+        question: 'Who is the President of US (2019)?',
+        a: 'Florin Pop',
+        b: 'Donald Trump',
+        c: 'Ivan Saldano',
+        d: 'Mihai Andrei',
+        correct: 'b'
+    },
+    {
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Markup Language',
+        b: 'Cascading Style Sheet',
+        c: 'Jason Object Notation',
+        d: 'Helicopters Terminals Motoboats Lamborginis',
+        correct: 'a'
+    },
+    {
+        question: 'What year was JavaScript launched?',
+        a: '1996',
+        b: '1995',
+        c: '1994',
+        d: 'None of the above',
+        correct: 'b'
+    }
+]
